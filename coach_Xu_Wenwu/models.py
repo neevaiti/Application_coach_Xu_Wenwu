afd74ad506f9579b3ef95e9c8842f3a69a7b866c
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-02-02 15:25:07 +0100

    Mise en place et configuration d'un premier model en tant que test. Ce model test initialise la db.

--- coach_Xu_Wenwu/models.py
+++ coach_Xu_Wenwu/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name = models.fields.CharField(max_length=100)
+    surname = models.fields.CharField(max_length=100)
+    email = models.fields.EmailField(max_length=300)
+    password = models.fields.CharField(max_length=150)
