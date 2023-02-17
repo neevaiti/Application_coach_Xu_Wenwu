f28e0e7f4c54ce3eaa92b15034c44a3fbd91685c
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-02-17 20:32:14 +0100

    Derniers petits ajustements.

--- coach_Xu_Wenwu/urls.py
+++ coach_Xu_Wenwu/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.signup, name='signup'),
-    path('sign-in/', views.signin, name='sign-in'),
-    path('sign-out/', views.signout, name='sign-out'),
+    path('sign-in/', views.signin, name='signin'),
+    path('sign-out/', views.signout, name='signout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('appointment/', views.book_appointment, name='book_appointment'),
-    path('history-appointment/', views.view_appointments, name='view_appointment'),
+    path('history-appointment/', views.view_appointments, name='view_appointments'),
     path('history-appointment/<str:patient_name>/', views.patient_appointments, name="patient_appointments"),
-    path('edit-notes/<int:appointment_id>/', views.edit_notes, name='edit_note'),
-    path('clients-list/', views.general_appointments_view, name='general_appointment')
+    path('edit-notes/<int:appointment_id>/', views.edit_notes, name='edit_notes'),
+    path('clients-list/', views.general_appointments_view, name='general_appointments_view')
 ]
